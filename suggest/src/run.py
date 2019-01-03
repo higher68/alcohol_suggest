37bfdf6734515845d70a7b4ad2e271569e768689
junya <hatiro7code@gmail.com>
2019-01-03 18:00:41 +0900

    20180103 アプリ起動の.py作成

---
+++ suggest/src/run.py
@@ -0,0 +1,15 @@
+import os
+
+from alcohol_suggest_api import create_app
+
+"""
+suggestAPIアプリケーション起動
+"""
+conf_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'conf')
+logging_conf_path = os.path.join(conf_dir, 'logging.conf')
+
+flask_env = os.getenv('FLASK_ENV', 'production')
+app = create_app(flask_env, logging_conf_path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=app.config['PORT'])
