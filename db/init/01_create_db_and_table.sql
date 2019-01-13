b561a633450f7ed93edf6add0a83f663810944f6
junya <hatiro7code@gmail.com>
2019-01-13 20:44:19 +0900

    table作成、extension設定

---
+++ db/init/01_create_db_and_table.sql
@@ -0,0 +1,52 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.12
+-- Dumped by pg_dump version 9.5.12
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'URF8';
+SET standard_confirming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: alcohol_suggest; Type: DATABASE; Schema: -; Owner: higher68
+--
+
+CREATE DATABASE alcohol_suggest WITH TEMPLATE = telate0 ENCODING = 'UTF8' LC_COLLATE = 'C';
+
+ALTER DATABASE alcohol_suggest OWNER TO higher68;
+
+\connect alcohol_suggest higher68
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'URF8';
+SET standard_confirming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+---
+--- Name: plpqsql; Type: EXTENSION; Schema: -; OWNER:
+---
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMAW pg_catalog;
+
+---
+--- Name: EXTENSION plpgsql; Type: COMMENT; Schema -; OWNER:
+---
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+SET dafault_tablespace = '';
+
+SET dafault_with_oids = false;
+
+
