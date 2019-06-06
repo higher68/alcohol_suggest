04f5c17c99144bbe1c519334b2c1ea2531c318a6
junya <hatiro7code@gmail.com>
2019-06-07 00:01:35 +0900

    レスポンス作成完了

--- src/alcohol_suggest_api/common_method.py
+++ src/alcohol_suggest_api/common_method.py
@@ -1,4 +1,11 @@
+
 from contextlib import contextmanager
+
+from api.classes.alcoloh_suggest import ErrorResponse
+
+from dataclass import asdict
+
+from flask import jsonify, make_response
 
 from sqlalchemy.orm import sessionmaker
 
@@ -11,12 +18,20 @@ def create_response(res=None, status_code=200):
 
     Parameters
     ----------
-
+    res: class
+        レスポンスクラス
+    status_code: int
+        ステータスコード
 
     Returns
     -------
+    flask.wrappers.Response
+        レスポンス
     """
-    pass
+    if res is None:
+        res = {}
+        return make_response(jsonify(res), status_code)
+    return make_response(jsonify(asdict(res)), status_code)
 
 
 def create_error_responze(error_message, status_code):
@@ -25,10 +40,18 @@ def create_error_responze(error_message, status_code):
 
     Parameters
     ----------
+    error_message: str
+        エラーメッセージ
+    status_code: int
+        ステータスコード
 
     Returns
     -------
+    flask.wrappers.Response
+        レスポンス
     """
+    res = ErrorResponse(error_message)
+    return create_response(res, status_code)
 
 
 @contextmanager
